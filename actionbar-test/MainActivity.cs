439f3337fc6f731e4db17aac06a35ce4b6b6e245
erics-mac <erics-mac@eric-macs-MacBook-Pro.local>
2015-10-05 19:52:22 -0500

    initial

---
+++ actionbar-test/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace actionbartest
+{
+    [Activity(Label = "actionbar-test", MainLauncher = true, Icon = "@drawable/icon", Theme="@style/CustomActionBarTheme")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            ActionBar.SetCustomView(Resource.Layout.ActionBar);
+            ActionBar.SetDisplayShowCustomEnabled(true);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var addButton = (LinearLayout)ActionBar.CustomView.FindViewById<LinearLayout>(Resource.Id.add);
+
+            addButton.Click += (object sender, System.EventArgs e) => 
+            {
+                var x = 5;
+            };  
+        }
+    }
+}
+
+
